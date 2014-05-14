0b13e6217a2c991293d80337775c71ab9879b2aa
Koushik Dasika <kdasika@koushikdasika.com>
2014-05-14 00:15:17 -0400

    better

--- matcher.go
+++ matcher.go
@@ -3,6 +3,7 @@ package main
 import (
   "github.com/garyburd/redigo/redis"
   "fmt"
+  "encoding/json"
 )
 
 func getFactsFromRedis(conn redis.Conn, order_id string) string {
@@ -14,22 +15,28 @@ func getFactsFromRedis(conn redis.Conn, order_id string) string {
 }
 
 func main() {
-  /*
   conn, err := redis.Dial("tcp", ":6379")
   if err != nil {
         // handle error
   }
-
-  conn.Do("APPEND", "1",
-  `[{"id":1, "type":"CheckoutItemSelected", "sku":"E031", "amount":100},
-    {"id":2, "type":"CheckoutItemSelected", "sku":"E031", "amount":100}]`)
-
-  facts := getFactsFromRedis(conn, "1")
-
-  fmt.Println("facts are %s\n", facts)
   defer conn.Close()
-  */
 
+  conn.Do("SET", 1, "[{\"type\":\"A\",\"id\":5,\"sku\":\"E031\",\"amount\":100},{\"type\":\"B\",\"id\":6,\"sku\":\"E032\",\"amount\":200}]")
+  facts_json := getFactsFromRedis(conn, "1")
+
+  fmt.Printf("facts are %s\n", facts_json)
+  var facts []map[string]interface{}
+  err = json.Unmarshal([]byte(facts_json), &facts)
+  if err != nil {
+    fmt.Println("error:", err)
+  }
+
+  for i := range facts {
+    fmt.Println(facts[i])
+  }
+
+
+  /*
   blah := []map[string]interface{} {
     map[string]interface{} {
       "id": 1,
@@ -48,5 +55,6 @@ func main() {
       fmt.Printf("for %dth fact, for key %s value %s\n", i, k, v)
     }
   }
+  */
 
 }
