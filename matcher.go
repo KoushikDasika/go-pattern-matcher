338b3ea4f15e5c2a1fba2aac31fa0f20e0b84e84
Koushik Dasika <kdasika@koushikdasika.com>
2014-05-23 17:35:10 -0400

    more tests

--- matcher.go
+++ matcher.go
@@ -5,6 +5,7 @@ import (
   //"encoding/json"
 )
 
-func MatchAttributes(facts []map[string]interface{}) {
+func MatchAttributes(facts []map[string]interface{}, attributes map[string]interface{}) {
   fmt.Println(facts)
+  fmt.Println(attributes)
 }
--- matcher_test.go
+++ matcher_test.go
@@ -7,9 +7,10 @@ import (
 var facts = []map[string]interface{}{
   {"id": 1, "type": "A", "sku": "E031", "amount": 100},
   {"id": 2, "type": "B", "sku": "E032", "amount": 200},
-  {"id": 3, "type": "C", "sku": "E033", "amount": 300},
-  {"id": 4, "type": "D", "sku": "E034", "amount": 400},
-  {"id": 5, "type": "E", "sku": "E035", "amount": 500},
+  {"id": 3, "type": "A", "sku": "E033", "amount": 300},
+  {"id": 4, "type": "B", "sku": "E034", "amount": 400},
+  {"id": 5, "type": "A", "sku": "E035", "amount": 500},
+  {"id": 6, "type": "B", "sku": "E035", "amount": 500},
 }
 
 type testpair struct {
@@ -17,7 +18,23 @@ type testpair struct {
   output []map[string]interface{}
 }
 
+var matching_tests = []testpair{
+  { map[string]interface{} {"type": "A"} ,  []map[string]interface{} {
+    {"id": 1, "type": "A", "sku": "E031", "amount": 100},
+    {"id": 3, "type": "A", "sku": "E033", "amount": 300},
+    {"id": 5, "type": "A", "sku": "E035", "amount": 500},
+  }},
+}
 
 func TestMatchAttributes(t *testing.T) {
-  MatchAttributes(facts)
+  for _, pair := range matching_tests {
+    v := MatchAttributes(facts, pair.matches)
+    if v != pair.output {
+      t.Error(
+        "For", pair.matches,
+        "expected", pair.output,
+        "got", v,
+      )
+    }
+  }
 }
