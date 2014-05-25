ba44ca9e26790eb6052bcc0fdbe7e61b09127eb0
Koushik Dasika <kdasika@koushikdasika.com>
2014-05-25 17:04:52 -0400

    added a few more operators

--- matcher.go
+++ matcher.go
@@ -4,15 +4,39 @@ import (
   "fmt"
 )
 
-func MatchAttributes(facts []map[string]interface{}, attributes map[string]map[string]interface{}) []map[string]interface{} {
-  var output []map[string]interface{}
+func MatchAttributes(facts []map[string]string, attributes map[string]map[string]string) []map[string]string {
+  var output []map[string]string
   for _, fact := range facts {
     flag := true
     for k, v := range attributes {
       switch v["operator"] {
         case "==":
           if (fact[k] != v["value"]) {
-            fmt.Println(v)
+            flag = false
+            break
+          }
+        case "!=":
+          if (fact[k] == v["value"]) {
+            flag = false
+            break
+          }
+        case "gt":
+          if (fact[k] <= v["value"]) {
+            flag = false
+            break
+          }
+        case "gte":
+          if (fact[k] < v["value"]) {
+            flag = false
+            break
+          }
+        case "lt":
+          if (fact[k] >= v["value"]) {
+            flag = false
+            break
+          }
+        case "lte":
+          if (fact[k] > v["value"]) {
             flag = false
             break
           }
--- matcher_test.go
+++ matcher_test.go
@@ -4,25 +4,50 @@ import (
   "testing"
 )
 
-var facts = []map[string]interface{}{
-  {"id": 1, "type": "A", "sku": "E031", "amount": 100},
-  {"id": 2, "type": "B", "sku": "E032", "amount": 200},
-  {"id": 3, "type": "A", "sku": "E033", "amount": 300},
-  {"id": 4, "type": "B", "sku": "E034", "amount": 400},
-  {"id": 5, "type": "A", "sku": "E035", "amount": 500},
-  {"id": 6, "type": "B", "sku": "E035", "amount": 500},
+var facts = []map[string]string{
+  {"id": "1", "type": "A", "sku": "E031", "amount": "100"},
+  {"id": "2", "type": "B", "sku": "E032", "amount": "200"},
+  {"id": "3", "type": "A", "sku": "E033", "amount": "300"},
+  {"id": "4", "type": "B", "sku": "E034", "amount": "400"},
+  {"id": "5", "type": "A", "sku": "E035", "amount": "500"},
+  {"id": "6", "type": "B", "sku": "E035", "amount": "600"},
 }
 
 type matching_test_pair struct {
-  matches map[string]map[string]interface{}
-  output []map[string]interface{}
+  matches map[string]map[string]string
+  output []map[string]string
 }
 
 var matching_tests = []matching_test_pair{
-  { map[string]map[string]interface{} {"type": {"operator": "==", "value": "A"}} ,  []map[string]interface{} {
-    {"id": 1, "type": "A", "sku": "E031", "amount": 100},
-    {"id": 3, "type": "A", "sku": "E033", "amount": 300},
-    {"id": 5, "type": "A", "sku": "E035", "amount": 500},
+  { map[string]map[string]string {"type": {"operator": "==", "value": "A"}} ,  []map[string]string {
+    {"id": "1", "type": "A", "sku": "E031", "amount": "100"},
+    {"id": "3", "type": "A", "sku": "E033", "amount": "300"},
+    {"id": "5", "type": "A", "sku": "E035", "amount": "500"},
+  }},
+  { map[string]map[string]string {"type": {"operator": "!=", "value": "A"}} ,  []map[string]string {
+    {"id": "2", "type": "B", "sku": "E032", "amount": "200"},
+    {"id": "4", "type": "B", "sku": "E034", "amount": "400"},
+    {"id": "6", "type": "B", "sku": "E035", "amount": "600"},
+  }},
+  { map[string]map[string]string {"id": {"operator": "gt", "value": "3"}} ,  []map[string]string {
+    {"id": "4", "type": "B", "sku": "E034", "amount": "400"},
+    {"id": "5", "type": "A", "sku": "E035", "amount": "500"},
+    {"id": "6", "type": "B", "sku": "E035", "amount": "600"},
+  }},
+  { map[string]map[string]string {"id": {"operator": "gte", "value": "3"}} ,  []map[string]string {
+    {"id": "3", "type": "A", "sku": "E033", "amount": "300"},
+    {"id": "4", "type": "B", "sku": "E034", "amount": "400"},
+    {"id": "5", "type": "A", "sku": "E035", "amount": "500"},
+    {"id": "6", "type": "B", "sku": "E035", "amount": "600"},
+  }},
+  { map[string]map[string]string {"id": {"operator": "lt", "value": "3"}} ,  []map[string]string {
+    {"id": "1", "type": "A", "sku": "E031", "amount": "100"},
+    {"id": "2", "type": "B", "sku": "E032", "amount": "200"},
+  }},
+  { map[string]map[string]string {"id": {"operator": "lte", "value": "3"}} ,  []map[string]string {
+    {"id": "1", "type": "A", "sku": "E031", "amount": "100"},
+    {"id": "2", "type": "B", "sku": "E032", "amount": "200"},
+    {"id": "3", "type": "A", "sku": "E033", "amount": "300"},
   }},
 }
 
