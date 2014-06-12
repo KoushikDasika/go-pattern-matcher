3f357fe4c58c9b566786c7b9522c10235a9eea9e
Koushik Dasika <kdasika@koushikdasika.com>
2014-06-12 17:15:25 -0500

    blah

--- matcher.go
+++ matcher.go
@@ -4,41 +4,53 @@ import (
   "fmt"
 )
 
-func MatchAttributes(facts []map[string]string, attributes map[string]map[string]string) []map[string]string {
-  var output []map[string]string
+func MatchAttributes(facts []map[string]interface{}, attributes map[string]map[string]interface{}) []map[string]interface{} {
+  var output []map[string]interface{}
   for _, fact := range facts {
     flag := true
     for k, v := range attributes {
       switch v["operator"] {
         case "==":
-          if (fact[k] != v["value"]) {
-            flag = false
-            break
+          for _, val := range v["value"] {
+            if (fact[k] != val) {
+              flag = false
+              break
+            }
           }
         case "!=":
-          if (fact[k] == v["value"]) {
-            flag = false
-            break
+          for _, val := range v["value"] {
+            if (fact[k] == val) {
+              flag = false
+              break
+            }
           }
         case "gt":
-          if (fact[k] <= v["value"]) {
-            flag = false
-            break
+          for _, val := range v["value"] {
+            if (fact[k] <= val) {
+              flag = false
+              break
+            }
           }
         case "gte":
-          if (fact[k] < v["value"]) {
-            flag = false
-            break
+          for _, val := range v["value"] {
+            if (fact[k] < val) {
+              flag = false
+              break
+            }
           }
         case "lt":
-          if (fact[k] >= v["value"]) {
-            flag = false
-            break
+          for _, val := range v["value"] {
+            if (fact[k] >= val) {
+              flag = false
+              break
+            }
           }
         case "lte":
-          if (fact[k] > v["value"]) {
-            flag = false
-            break
+          for _, val := range v["value"] {
+            if (fact[k] > val) {
+              flag = false
+              break
+            }
           }
         default:
           fmt.Println("Pick a better operator, foo")
